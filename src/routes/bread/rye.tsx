8b0b97653669740b15f43b8cf775f4a0cf1a5b14
Davyn Cox <97019353+DavynCox@users.noreply.github.com>
2023-04-14 17:43:00 -0700

    added styled component dependency, fixed rpc issue

--- src/routes/bread/rye.tsx
+++ src/routes/bread/rye.tsx
@@ -1,4 +1,10 @@
-import { logOnServer } from "~/lib/rpc";
+import server$ from "solid-start/server";
+
+// this only logs on the server, not on the client
+// https://start.solidjs.com/api/server
+export const logOnServer = server$(async (message) => {
+  console.log(message);
+});
 
 export default function Rye() {
   return <h1 onClick={() => logOnServer("rye")}>Rye</h1>;
